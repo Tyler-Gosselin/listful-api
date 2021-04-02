ce2321c97bb00f5a92834d4ba0b76ea90330f55c
Brandon Ward <brandonward.gk@gmail.com>
2021-04-02 10:30:22 -0600

    deleted commented code

--- app/routes.py
+++ app/routes.py
@@ -23,18 +23,6 @@ def create_user():
     else:
         return jsonify(user_schema.dump(new_user))
 
-# @app.route('/create-user', methods=['POST'])
-# def create_user():
-#     username = request.json['username']
-#     email = request.json['email']
-#     password = request.json['password']
-#     if User.query.filter_by(username=username).first() is None:
-#         new_user = User(username=username, email=email, password=password)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         return jsonify(user_schema.dump(new_user))
-#     return jsonify({"message": "Username is taken"})
-
 @app.route('/users')
 def get_users():
     users = User.query.all()
